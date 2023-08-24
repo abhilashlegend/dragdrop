1a0eaa11f37f1f35a12c051096066201dd6dc286
abhilashlegend <abhilash456a@live.com>
2023-08-24 17:10:08 +0530

    Added DOM interaction

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionElement: HTMLInputElement;
+    peopleElement: HTMLInputElement;
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -10,10 +13,23 @@ class ProjectInput {
         this.element = importedNode.firstElementChild as HTMLFormElement;
         this.element.id = 'user-input';
         this.attach();
+        this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+        this.descriptionElement = this.element.querySelector("#description") as HTMLInputElement;
+        this.peopleElement = this.element.querySelector("#people") as HTMLInputElement;
+        this.configure();
     }
 
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
 }
 
