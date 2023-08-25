72fceee4fe89754afd0e43fbae699b08e5a5eb9a
abhilashlegend <abhilash456a@live.com>
2023-08-25 12:11:22 +0530

    Added validation method and interface

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,34 @@
+// Validation
+
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?:number;
+}
+
+function validate(validatableInput: Validatable):boolean {
+    let isValid = true;
+    if(validatableInput.required){
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if(validatableInput.minLength != null && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+    }
+    if(validatableInput.maxLength != null && typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+    }
+    if(validatableInput.min != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if(validatableInput.max != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -23,9 +54,56 @@ class ProjectInput {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 
+    private clearInputs() {
+        this.titleInputElement.value = '';
+        this.descriptionElement.value = '';
+        this.peopleElement.value = '';
+    }
+
     private submitHandler(event: Event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if(Array.isArray(userInput)){
+            const [title, desc, people] = userInput;
+            console.log(title, desc, people);
+            this.clearInputs();
+        }
+       
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDesc = this.descriptionElement.value;
+        const enteredPeople = this.peopleElement.value;
+
+        const validateTitle:Validatable = {
+            value: enteredTitle,
+            required: true,
+            minLength: 3,
+            maxLength: 30,
+        }
+
+        const validateDescription:Validatable = {
+            value: enteredDesc,
+            required: true,
+            minLength: 5,
+        }
+        const validatePeople:Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        }
+
+
+        if(!validate(validateTitle) ||
+           !validate(validateDescription) ||
+           !validate(validatePeople)){
+            alert("Invalid input, please try again!");
+            return;
+           } else {
+            return [enteredTitle, enteredDesc, +enteredPeople]
+           }
     }
 
     private configure() {
