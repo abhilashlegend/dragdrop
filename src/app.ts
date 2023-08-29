e1f4835a65b83afcd01896f90c48f248ad6445aa
abhilashlegend <abhilash456a@live.com>
2023-08-29 11:45:30 +0530

    Added getter to o/p  no.of persons

--- src/app.ts
+++ src/app.ts
@@ -159,6 +159,14 @@ enum ProjectStatus {
   class ProjectItem extends Component<HTMLUListElement, HTMLElement> {
     private project: Project;
 
+    getPerson() {
+      if(this.project.people === 1){
+        return "1 person";
+      } else {
+        return `${this.project.people} persons`;
+      }
+    }
+
     constructor(hostId: string, project: Project){
       super('single-project', hostId, false, project.id);
       this.project = project;
@@ -174,7 +182,7 @@ enum ProjectStatus {
 
     renderContent(): void {
       this.element.querySelector('h2')!.textContent = this.project.title;
-      this.element.querySelector('h3')!.textContent = this.project.people.toString();
+      this.element.querySelector('h3')!.textContent = this.getPerson() + ' assigned';
       this.element.querySelector('p')!.textContent = this.project.description;
     }
 
