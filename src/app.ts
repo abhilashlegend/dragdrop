442204297571110308ad19ea28adc4ef8c2c26da
abhilashlegend <abhilash456a@live.com>
2023-08-29 14:32:18 +0530

    Added dropable area

--- src/app.ts
+++ src/app.ts
@@ -189,7 +189,8 @@ enum ProjectStatus {
     }
 
     dragStartHandler(event: DragEvent): void {
-      console.log(event);
+      event.dataTransfer!.setData('text/plain', this.project.id);
+      event.dataTransfer!.effectAllowed = 'move';
     }
 
     dragEndHandler(_: DragEvent): void {
@@ -221,13 +222,18 @@ enum ProjectStatus {
       this.renderContent();
     }
 
-    dragOverHandler(_: DragEvent): void {
-      const listEl = this.element.querySelector('ul')!
-      listEl.classList.add('droppable');
+    dragOverHandler(event: DragEvent): void {
+     
+      if(event.dataTransfer && event.dataTransfer.types[0] === 'text/plain'){
+        event.preventDefault();
+        const listEl = this.element.querySelector('ul')!;
+        listEl.classList.add('droppable');
+      }
+      
     }
 
-    dropHandler(_: DragEvent): void {
-      
+    dropHandler(event: DragEvent): void {
+      console.log(event.dataTransfer?.getData('text/plain'));
     }
 
     dragLeaveHandler(_: DragEvent): void {
